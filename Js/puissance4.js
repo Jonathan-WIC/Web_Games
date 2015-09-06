508548bc299fca30ebad174f64c4c7712a132629
h1103402 <jonathh@hotmail.fr>
2015-09-06 16:26:25 +0200

    Commentaire dans le JS du puissance 4

--- Js/puissance4.js
+++ Js/puissance4.js
@@ -1,6 +1,8 @@
 var player, winner, cpt;
 $(document).ready(function() {
 	cpt = 0;
+
+	//Cache le message de fin de jeu et son container
 	$('#end_layer').hide();
 
 	$('.cell').click(function(){
@@ -13,23 +15,32 @@ $(document).ready(function() {
 });
 
 function selectCell(cell){
+	//Recup le nombre de coup joué (max 42)
 	cpt++;
+	//Recup le joueur courant et assigne la classe associée
 	player = parseInt($('.puissance4_controler').attr("player"));
-	console.log(player);
 	if (player == 1)
 		cell.removeClass('player_2').addClass('player_1');
 	else	
 		cell.removeClass('player_1').addClass('player_2');
 	
+	//Vérifie si le joueur courant a gagné
 	testVictory(cell.data('cell'), player);
+	//Empeche de recliquer sur la cellule
 	cell.unbind('click');
+
+	//Change de joueur
 	$('.puissance4_controler').attr("player", (player+1)%2);
 }
 
 function clearGrid(){
 	cpt = 0;
 	winner = false;
+
+	//Le joeuur 1 commence toujours
 	$('.puissance4_controler').attr("player", 1);
+
+	//Remet à 0 chaque cellule
 	$('.cell').each(function(index, el) {
 		$(this).off('click'); //unbind event to avoid the event called twice
 		$(this).removeClass('player_1').removeClass('player_2');
@@ -37,14 +48,17 @@ function clearGrid(){
 			selectCell($(this));
 		})
 	});
+
+	//Cache le layer de fin
 	$('#end_layer').fadeOut('fast');
 }
 
 function testVictory(cell, currentPlayer){
+	//currentPlayer est égale à 1 ou à 0  ===>  le joueur 1 correspond à 1 et le joueur 2 a 0 (d'où la condition suivante)
 	if (currentPlayer == 0)
 		currentPlayer = 2;
 
-
+	//Si il y a 42 coups, alors fin du jeu car égalité
 	if(cpt == 42){
 		$('#end_msg').text("Spare");
 		$('#end_layer').fadeIn('fast');
